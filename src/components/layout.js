c1467fbc1b54483f7982a55709fd7e7b2b9da0e1
Tehseen Afzal <abuashja@gmail.com>
2022-01-22 00:53:30 +0500

    Add graphql query

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,30 @@
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import * as React from 'react';
 import {
     container,
     heading,
     navLinks,
     navLinkItem,
-    navLinkText
+    navLinkText,
+    siteTitle
 } from './layout.module.css'
 
 const Layout = ({pageTitle, children}) => {
+
+    const data = useStaticQuery(graphql`
+    query MyQuery {
+        site{
+              siteMetadata{
+                  title
+          }
+        }
+      }
+    `)
+
     return(
         <div className={container}>
-            <title>{pageTitle}</title>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}>
@@ -19,6 +32,9 @@ const Layout = ({pageTitle, children}) => {
                     </li>
                     <li className={navLinkItem}>
                         <Link to="/about" className={navLinkText}>About</Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog" className={navLinkText}>Blog</Link>
                     </li>
                 </ul>
             </nav>
