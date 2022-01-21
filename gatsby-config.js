b0f53c8e8ad5a0067f4c17fb803ccbdfddd30501
Tehseen Afzal <abuashja@gmail.com>
2022-01-22 00:53:07 +0500

    configure source file system plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
     siteMetadata: {
+        title: `My Blog for Cars`,
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
         "gatsby-plugin-image",
         "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: `blog`,
+                path: `${__dirname}/blog`,
+            }
+        }
     ]
 }
